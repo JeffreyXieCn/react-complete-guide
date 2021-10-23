05d8e128f72e73660a922175e884c30e5090b612
Jeffrey XIE <jeffrey.xie@unity3d.com>
2021-10-23 19:22:05 -0400

    Add users list component and make adding user work

--- 03-user-management/src/App.js
+++ 03-user-management/src/App.js
@@ -1,9 +1,36 @@
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+import { useState } from "react";
+
+const INITIAL_USERS = [
+  {
+    id: "u1",
+    name: "Jeff",
+    age: 37,
+  },
+  {
+    id: "u2",
+    name: "Max",
+    age: 31,
+  },
+];
 
 function App() {
+  const [usersList, setUsersList] = useState(INITIAL_USERS);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
