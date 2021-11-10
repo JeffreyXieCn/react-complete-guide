c37fa69dc921c99dbf49633be0ef4268e93afec4
Jeffrey XIE <jeffrey.xie@unity3d.com>
2021-11-09 20:41:43 -0500

    Practice hook useReducer

--- 11-work-with-forms/src/hooks/use-input.js
+++ 11-work-with-forms/src/hooks/use-input.js
@@ -1,33 +1,52 @@
-import {useState} from "react";
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === "BLUR") {
+    return { isTouched: true, value: state.value };
+  }
+  // if (action.type === "RESET") {
+  //   return { isTouched: false, value: "" };
+  // }
+  return initialInputState;
+};
 
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState('');
-  const [enteredValueTouched, setEnteredValueTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
 
-  const enteredValueIsValid = validateValue(enteredValue);
-  const hasError = !enteredValueIsValid && enteredValueTouched;
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
   const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
+    dispatch({ type: "INPUT", value: event.target.value });
   };
 
   const inputBlurHandler = (event) => {
-    setEnteredValueTouched(true);
+    dispatch({ type: "BLUR" });
   };
 
   const reset = () => {
-    setEnteredValue("");
-    setEnteredValueTouched(false);
+    dispatch({ type: "RESET" });
   };
 
   return {
-    value: enteredValue,
-    isValid: enteredValueIsValid,
+    value: inputState.value,
+    isValid: valueIsValid,
     hasError,
     valueChangeHandler,
     inputBlurHandler,
-    reset
-  }
+    reset,
+  };
 };
 
 export default useInput;
