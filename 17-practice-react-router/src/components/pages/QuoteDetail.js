3832952900bcdca32cc9a2d98d2a33075763c6cd
Jeffrey XIE <jeffrey.xie@unity3d.com>
2021-11-13 12:17:00 -0500

    Work on the quote detail page

--- 17-practice-react-router/src/components/pages/QuoteDetail.js
+++ 17-practice-react-router/src/components/pages/QuoteDetail.js
@@ -1,15 +1,33 @@
 import { Route, useParams } from "react-router-dom";
 import Comments from "../comments/Comments";
+import HighlightedQuote from "../quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Max",
+    text: "Learning React is fun!",
+  },
+  {
+    id: "q2",
+    author: "Jeff",
+    text: "Learning React is great!",
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>
+  }
 
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author}/>
       <Route path={`/quotes/${params.quoteId}/comments`}>
-        <Comments />
+        <Comments/>
       </Route>
     </>
   );
