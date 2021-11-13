c9fc0cf712f28662b0794ba921ecdd7bdda305fb
Jeffrey XIE <jeffrey.xie@unity3d.com>
2021-11-13 10:50:27 -0500

    Work with nested routes and redirect the user

--- 16-react-router/src/pages/Welcome.js
+++ 16-react-router/src/pages/Welcome.js
@@ -1,5 +1,14 @@
+import { Route } from "react-router-dom";
+
 const Welcome = () => {
-  return <h1>This is the welcome page</h1>;
-}
+  return (
+    <section>
+      <h1>This is the welcome page</h1>
+      <Route path="/welcome/new-user">
+        <p>Welcome new user!</p>
+      </Route>
+    </section>
+  );
+};
 
 export default Welcome;
