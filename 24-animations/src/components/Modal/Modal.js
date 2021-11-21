4e133d501697c7471a45a67aba9accd5093071ba
Jeffrey XIE <jeffrey.xie@unity3d.com>
2021-11-21 17:48:40 -0500

    Apply transition to modal window

--- 24-animations/src/components/Modal/Modal.js
+++ 24-animations/src/components/Modal/Modal.js
@@ -3,7 +3,14 @@ import React from "react";
 import "./Modal.css";
 
 const modal = (props) => {
-  const cssClasses = ["Modal", props.show ? "ModalOpen" : "ModalClosed"];
+  let transition = null;
+  if (props.show === "entering") {
+    transition = "ModalOpen";
+  } else if (props.show === "exiting") {
+    transition = "ModalClosed";
+  }
+
+  const cssClasses = ["Modal", transition];
 
   return (
     <div className={cssClasses.join(" ")}>
