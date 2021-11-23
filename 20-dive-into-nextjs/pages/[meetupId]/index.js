b9ad753ed55d3e05a79515cf7e2a5eb60b6e2be9
Jeffrey XIE <jeffrey.xie@unity3d.com>
2021-11-23 09:26:24 -0500

    Try Static Site Generation for meetup details page

--- 20-dive-into-nextjs/pages/[meetupId]/index.js
+++ 20-dive-into-nextjs/pages/[meetupId]/index.js
@@ -11,4 +11,43 @@ function MeetUpDetail() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://cw-gbl-gws-prod.azureedge.net/-/media/cw/americas/canada/office-pages/montreal-hero-desktop-new.jpg?rev=50d82a2a23914cb583d739bb486238ae",
+        id: meetupId,
+        title: "Fist Meetup",
+        address: "Some Street 5, Some City",
+        description: "This is a first meetup",
+      },
+    },
+  };
+}
+
 export default MeetUpDetail;
