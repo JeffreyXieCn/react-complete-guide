c9b97d0473fe5e42fcd9242090c1c959e0c6ce35
Jeffrey XIE <jeffrey.xie@unity3d.com>
2021-11-22 21:18:50 -0500

    Add custom components and CSS modules for Meetup Details

--- 20-dive-into-nextjs/pages/[meetupId]/index.js
+++ 20-dive-into-nextjs/pages/[meetupId]/index.js
@@ -1,5 +1,14 @@
-function MeetUpDetail(){
-  return <p>ToDo</p>
+import MeetUpDetails from "../../components/meetups/MeetupDetails";
+
+function MeetUpDetail() {
+  return (
+    <MeetUpDetails
+      image="https://cw-gbl-gws-prod.azureedge.net/-/media/cw/americas/canada/office-pages/montreal-hero-desktop-new.jpg?rev=50d82a2a23914cb583d739bb486238ae"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="The meetup description"
+    />
+  );
 }
 
 export default MeetUpDetail;
