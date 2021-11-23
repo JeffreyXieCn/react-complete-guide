f14d9cfba74d31ff7c6cba023cb5bfdcb5583ac4
Jeffrey XIE <jeffrey.xie@unity3d.com>
2021-11-22 21:43:29 -0500

    Try Static Site Generation with Home Page

--- 20-dive-into-nextjs/pages/index.js
+++ 20-dive-into-nextjs/pages/index.js
@@ -1,24 +1,36 @@
 import MeetupList from "../components/meetups/MeetupList";
-import Layout from "../components/layout/Layout";
 
 const DUMMY_MEETUPS = [
   {
-    id: 'm1',
-    title: 'A First Meetup',
-    image: 'https://cw-gbl-gws-prod.azureedge.net/-/media/cw/americas/canada/office-pages/montreal-hero-desktop-new.jpg?rev=50d82a2a23914cb583d739bb486238ae',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!'
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://cw-gbl-gws-prod.azureedge.net/-/media/cw/americas/canada/office-pages/montreal-hero-desktop-new.jpg?rev=50d82a2a23914cb583d739bb486238ae",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
   },
   {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image: 'https://cw-gbl-gws-prod.azureedge.net/-/media/cw/americas/canada/office-pages/montreal-hero-desktop-new.jpg?rev=50d82a2a23914cb583d739bb486238ae',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a second meetup!'
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://cw-gbl-gws-prod.azureedge.net/-/media/cw/americas/canada/office-pages/montreal-hero-desktop-new.jpg?rev=50d82a2a23914cb583d739bb486238ae",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a second meetup!",
   },
 ];
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />
+
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 10, // seconds
+  };
 }
 
 export default HomePage;
