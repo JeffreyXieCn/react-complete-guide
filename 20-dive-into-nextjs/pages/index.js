74fb23d2c9d1cce11b202d4c98ff9761cadeb8b3
Jeffrey XIE <jeffrey.xie@unity3d.com>
2021-11-23 11:02:26 -0500

    Fetch all meetups from MongoDB to pre-render home page

--- 20-dive-into-nextjs/pages/index.js
+++ 20-dive-into-nextjs/pages/index.js
@@ -1,4 +1,5 @@
 import MeetupList from "../components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
 
 const DUMMY_MEETUPS = [
   {
@@ -37,9 +38,24 @@ function HomePage(props) {
 
 export async function getStaticProps() {
   // fetch data from an API, the code here runs on the server, never leaks to user's browser
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://xwy5201314:npKZuTN82ivi3THC@cluster4nextjs.2c0ud.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("myMeetups");
+  const meetups = await meetupsCollection.find().toArray();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      //meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 10, // seconds
   };
